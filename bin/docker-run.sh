cbc72a2386444b8e4a0c13e0a14486705c93dd3e
Peter Lyons <pete@peterlyons.com>
2018-07-11 05:14:48 -0600

    Tweak docker settings trying to work offline

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -17,5 +17,6 @@ cd "$(dirname "$0")/.."
 exec docker run --rm --interactive --tty \
   --volume "${PWD}:/opt" --workdir /opt \
   --env USER=root \
-  --env PATH=/usr/local/cargo/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/target/debug \
+  --env PATH=/opt/.cargo/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/target/debug \
+  --env CARGO_HOME=/opt/.cargo \
   rust:1.27 "${1-/bin/bash}"
