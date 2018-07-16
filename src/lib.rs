db17cf58831f45b5624a22f0d4da72cf31f48b3c
Peter Lyons <pete@peterlyons.com>
2018-07-15 23:46:08 -0600

    Extract functions to lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+extern crate base64;
+extern crate byteorder;
+extern crate hex;
+pub mod error;
+use byteorder::{BigEndian, ReadBytesExt};
+use error::{exit, CodyError};
+use std::io::Cursor;
+
+fn trim(encoded_input: Vec<u8>) -> Result<String, std::string::FromUtf8Error> {
+    // Strip leading and trailing ASCII whitespace
+    let in_string = String::from_utf8(encoded_input)?;
+    Ok(in_string.trim().into())
+}
+
+pub fn binary_base64(input: &Vec<u8>) -> String {
+    base64::encode(input)
+}
+
+pub fn binary_hex(input: &Vec<u8>) -> String {
+    hex::encode(input)
+}
+
+pub fn hex_dec(encoded_input: Vec<u8>) -> Result<u64, CodyError> {
+    let in_string = trim(encoded_input)?;
+    let decoded_input = hex::decode(in_string.as_bytes())?;
+    if decoded_input.len() > 8 {
+        return exit("Can only decode a maximum of 16 hexadecimal characters to decimal").map(|_| 0);
+    }
+    binary_decimal(decoded_input)
+}
+
+pub fn binary_decimal(mut input: Vec<u8>) -> Result<u64, CodyError> {
+    if input.len() > 8 {
+        return exit("Can only decode a maximum of 8 bytes to decimal").map(|_| 0);
+    }
+    // Pad with leading zero bytes until we have 64 bits total
+    while input.len() < 8 {
+        input.insert(0, 0);
+    }
+
+    let mut reader = Cursor::new(&input);
+    Ok(reader.read_u64::<BigEndian>()?)
+}
+
+pub fn hex_binary(encoded_input: Vec<u8>) -> Result<Vec<u8>, CodyError> {
+    let in_string = trim(encoded_input)?;
+    let decoded_input = hex::decode(in_string.as_bytes())?;
+    Ok(decoded_input)
+}
+
+pub fn dec_hex(encoded_input: Vec<u8>) -> Result<u64, CodyError> {
+    let in_string = trim(encoded_input)?;
+    Ok(in_string.parse()?)
+}
+
+pub fn base64_hex(encoded_input: Vec<u8>) -> Result<String, CodyError> {
+    let in_string = trim(encoded_input)?;
+    let decoded_input = base64::decode(&in_string)?;
+    let mut output = String::new();
+    for byte in &decoded_input {
+        output.push_str(&format!("{:x}", byte));
+    }
+    Ok(output)
+}
+
+pub fn base64_binary(encoded_input: Vec<u8>) -> Result<Vec<u8>, CodyError> {
+    let in_string = trim(encoded_input)?;
+    Ok(base64::decode(&in_string)?)
+}
+
+pub fn hex_base64(encoded_input: Vec<u8>) -> Result<String, CodyError> {
+    let in_string = trim(encoded_input)?;
+    let decoded_input = hex::decode(&in_string)?;
+    Ok(base64::encode(&decoded_input))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,7 @@
-extern crate base64;
-extern crate byteorder;
-extern crate hex;
-mod error;
-use byteorder::{BigEndian, ReadBytesExt};
-use error::{exit, CodyError};
+extern crate cody;
+use cody::error::CodyError;
 use std::env::args;
-use std::io::{self, Cursor, Read, Write};
-
-fn trim(encoded_input: Vec<u8>) -> Result<String, std::string::FromUtf8Error> {
-    // Strip leading and trailing ASCII whitespace
-    let in_string = String::from_utf8(encoded_input)?;
-    Ok(in_string.trim().into())
-}
-
-fn hex_dec(encoded_input: Vec<u8>) -> Result<u64, CodyError> {
-    let in_string = trim(encoded_input)?;
-    let decoded_input = hex::decode(in_string.as_bytes())?;
-    if decoded_input.len() > 8 {
-        return exit("Can only decode a maximum of 16 hexadecimal characters to decimal").map(|_| 0);
-    }
-    binary_decimal(decoded_input)
-}
-
-fn binary_decimal(mut input: Vec<u8>) -> Result<u64, CodyError> {
-    if input.len() > 8 {
-        return exit("Can only decode a maximum of 8 bytes to decimal").map(|_| 0);
-    }
-    // Pad with leading zero bytes until we have 64 bits total
-    while input.len() < 8 {
-        input.insert(0, 0);
-    }
-
-    let mut reader = Cursor::new(&input);
-    Ok(reader.read_u64::<BigEndian>()?)
-}
-
-fn hex_binary(encoded_input: Vec<u8>) -> Result<Vec<u8>, CodyError> {
-    let in_string = trim(encoded_input)?;
-    let decoded_input = hex::decode(in_string.as_bytes())?;
-    Ok(decoded_input)
-}
-
-fn dec_hex(encoded_input: Vec<u8>) -> Result<u64, CodyError> {
-    let in_string = trim(encoded_input)?;
-    Ok(in_string.parse()?)
-}
-
-fn base64_hex(encoded_input: Vec<u8>) -> Result<String, CodyError> {
-    let in_string = trim(encoded_input)?;
-    let decoded_input = base64::decode(&in_string)?;
-    let mut output = String::new();
-    for byte in &decoded_input {
-        output.push_str(&format!("{:x}", byte));
-    }
-    Ok(output)
-}
-
-fn base64_binary(encoded_input: Vec<u8>) -> Result<Vec<u8>, CodyError> {
-    let in_string = trim(encoded_input)?;
-    Ok(base64::decode(&in_string)?)
-}
-
-fn hex_base64(encoded_input: Vec<u8>) -> Result<String, CodyError> {
-    let in_string = trim(encoded_input)?;
-    let decoded_input = hex::decode(&in_string)?;
-    Ok(base64::encode(&decoded_input))
-}
+use std::io::{self, Read, Write};
 
 fn main() -> Result<(), CodyError> {
     let mut stdin = io::stdin();
@@ -74,39 +10,39 @@ fn main() -> Result<(), CodyError> {
     let out_format = args().nth(2).unwrap_or("binary".into());
     let out_format = out_format.as_str();
     let mut encoded_input = vec![];
+
     stdin.read_to_end(&mut encoded_input)?;
     if encoded_input.len() == 0 {
-        return exit("standard input was empty");
+        return cody::error::exit("standard input was empty");
     }
-    println!("{} {}", in_format, out_format);
+
     match (in_format, out_format) {
         ("binary", "base64") => {
-            println!("{}", base64::encode(&encoded_input));
+            println!("{}", cody::binary_base64(&encoded_input));
         }
         ("binary", "decimal") => {
-            println!("{}", binary_decimal(encoded_input)?);
+            println!("{}", cody::binary_decimal(encoded_input)?);
         }
         ("binary", "hex") => {
-            println!("{}", hex::encode(&encoded_input));
+            println!("{}", cody::binary_hex(&encoded_input));
         }
         ("base64", "binary") => {
-            io::stdout().write(&base64_binary(encoded_input)?)?;
+            io::stdout().write(&cody::base64_binary(encoded_input)?)?;
         }
         ("base64", "hex") => {
-            println!("{}", base64_hex(encoded_input)?);
+            println!("{}", cody::base64_hex(encoded_input)?);
         }
         ("decimal", "hex") => {
-            println!("{:x}", dec_hex(encoded_input)?);
+            println!("{:x}", cody::dec_hex(encoded_input)?);
         }
         ("hex", "decimal") => {
-            println!("{}", hex_dec(encoded_input)?);
+            println!("{}", cody::hex_dec(encoded_input)?);
         }
         ("hex", "binary") => {
-            io::stdout().write(&hex_binary(encoded_input)?)?;
+            io::stdout().write(&cody::hex_binary(encoded_input)?)?;
         }
-
         ("hex", "base64") => {
-            println!("{}", hex_base64(encoded_input)?);
+            println!("{}", cody::hex_base64(encoded_input)?);
         }
         (_, _) => {
             io::stdout().write(&encoded_input)?;
